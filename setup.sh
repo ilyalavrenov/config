874707d6ca818601e65b362fad91bbec28994583
Ilya Lavrenov <ilyalavrenov@ilya-mbp.local>
2018-09-14 18:22:38 -0700

    initial commit

---
+++ setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+cd "$(dirname "$0")"
+
+chflags nohidden ~/Library
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder ShowStatusBar -bool true
+
+which -s brew; if [[ $? != 0 ]]; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    brew update
+fi
+
+if [ -f Brewfile ]; then
+    brew bundle install
+fi
+
+if [ ! -d ~/.oh-my-zsh ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+if [ -f .aliases ]; then
+    ln -sf .aliases ~/.aliases
+fi
+
+if [ -f .zshrc ]; then
+    ln -sf .zshrc ~/.zshrc
+fi
+
+if [ ! -f ~/Library/Fonts/Menlo\ for\ Powerline.ttf ]; then
+    curl -sLo fonts.zip https://github.com/abertsch/Menlo-for-Powerline/archive/master.zip
+    unzip -j fonts.zip -d fonts/
+    cp -f fonts/*.ttf ~/Library/Fonts/
+    rm -rf fonts*
+fi
+
+exec ${SHELL} -l
