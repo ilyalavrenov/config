c3007fc03238b0d27e118533ab17ef3fe9535dee
Ilya Lavrenov <17838283+ilyalavrenov@users.noreply.github.com>
2019-04-18 12:07:43 -0700

    update

--- setup.sh
+++ setup.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -uxo pipefail
+trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+IFS=$'\n\t'
 
 cd "$(dirname "$0")"
 
 chflags nohidden ~/Library
 
+osascript -e 'tell application "System Preferences" to quit'
+
+defaults write com.apple.ActivityMonitor ShowCategory -int 0
+defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
+defaults write com.apple.ActivityMonitor SortDirection -int 0
+
 defaults write com.apple.finder ShowPathbar -bool true
 defaults write com.apple.finder ShowStatusBar -bool true
 defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder NewWindowTarget -string "PfCm"
 defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 defaults write com.apple.finder QLEnableTextSelection -bool true
 
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
@@ -26,12 +36,28 @@ defaults write com.apple.dock wvous-bl-modifier -int 0
 defaults write com.apple.dock wvous-br-corner -int 7
 defaults write com.apple.dock wvous-br-modifier -int 0
 
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
 defaults write com.apple.menuextra.clock DateFormat -string "EEE MMM d  h:mm a"
 defaults write com.apple.menuextra.battery ShowPercent -bool true
 
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+if ! xcode-select --version; then
+    xcode-select --install
+fi
 
 if ! which -s brew; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -47,8 +73,19 @@ if [ -f .aliases ]; then
     ln -sf $(pwd)/.aliases ~/.aliases
 fi
 
-if [ -f .zshrc ]; then
-    ln -sf $(pwd)/.zshrc ~/.zshrc
+if [ -f .gitignore ]; then
+    ln -sf $(pwd)/.gitignore ~/.gitignore
+    git config --global user.name "ilya lavrenov"
+    git config --global user.email "17838283+ilyalavrenov@users.noreply.github.com"
+    git config --global core.excludesfile ~/.gitignore
+fi
+
+if [ ! -f ~/.ssh/id_rsa.pub ]; then
+    ssh-keygen -t rsa -b 4096
+    cat ~/.ssh/id_rsa.pub
+    echo "add this pubkey to github \n"
+    echo "https://github.com/account/ssh \n"
+    read -p "hit [enter] to continue..."
 fi
 
 if [ ! -f ~/Library/Fonts/Menlo\ for\ Powerline.ttf ]; then
